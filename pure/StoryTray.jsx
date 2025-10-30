c870515111351cbea93867c1518d9869705251e8
warubert <william.rubert@gmail.com>
2025-10-30 20:15:44 -0300

    Keeping Components Pure

---
+++ pure/StoryTray.jsx
@@ -0,0 +1,14 @@
+export default function StoryTray({ stories }) {
+  stories.push({
+    id: "create",
+    label: "Create Story",
+  });
+
+  return (
+    <ul>
+      {stories.map((story) => (
+        <li key={story.id}>{story.label}</li>
+      ))}
+    </ul>
+  );
+}
